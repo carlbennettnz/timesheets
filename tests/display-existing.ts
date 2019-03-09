5be2b43ebefe8e10b9ba8518b0a322c7e5b9b195
Carl Bennett <carlbennettnz@gmail.com>
2019-03-09 19:10:17 +1300

    Switch to Cypress for e2e testing

--- tests/display-existing.ts
+++
@@ -1,8 +0,0 @@
-import { Selector } from 'testcafe'
-
-fixture('Display existing').page(`${process.env.STAGING_SERVER}/`)
-
-test('Show no existing timesheets', async t => {
-  await t.expect(Selector('tr').count).eql(0)
-  await t.expect(Selector('p').innerText).eql(`There aren't any timesheet entries to display yet`)
-})
